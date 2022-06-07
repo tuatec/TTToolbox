99af4e1ef44383bcde676359d473807eff93bb9e
AchimTuran <mail@achim-turan.de>
2022-06-08 01:01:12 +0200

    introduce ik solver to constraint bones

---
+++ Source/TTToolbox/Private/IKRig_ConstraintBones.cpp
@@ -0,0 +1,97 @@
+// The MIT License (MIT)
+// ---------------------
+// 
+// Copyright 2022 Achim Turan (https://www.instagram.com/tuatec/)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+// THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include "IKRig_ConstraintBones.h"
+
+#define LOCTEXT_NAMESPACE "UIKRig_BoneConstrainer"
+
+UIKRig_ConstraintBones::UIKRig_ConstraintBones() {}
+UIKRig_ConstraintBones::~UIKRig_ConstraintBones() {}
+
+void UIKRig_ConstraintBones::Initialize(const FIKRigSkeleton& IKRigSkeleton)
+{
+	m_constraintBones.Reserve(ConstraintBones.Num());
+
+	//! @todo @ffs sort bone indices
+	for (auto& constraintBone : ConstraintBones)
+	{
+		m_constraintBones.Add(
+			{
+				IKRigSkeleton.GetBoneIndexFromName(constraintBone.ConstraintBone),
+				IKRigSkeleton.GetBoneIndexFromName(constraintBone.ModifiedBone)
+			});
+	}
+}
+
+void UIKRig_ConstraintBones::Solve(FIKRigSkeleton& IKRigSkeleton, const FIKRigGoalContainer& Goals)
+{
+	if (m_constraintBones.Num() <= 0)
+	{
+		// nothing to do here as no constraint bones are configured
+		return;
+	}
+
+	for (auto& constraintBone : m_constraintBones)
+	{
+		IKRigSkeleton.CurrentPoseGlobal[constraintBone.ModifiedBone] = IKRigSkeleton.CurrentPoseGlobal[constraintBone.ConstraintBone];
+		IKRigSkeleton.PropagateGlobalPoseBelowBone(constraintBone.ModifiedBone);
+	}
+}
+
+#if WITH_EDITOR
+
+FText UIKRig_ConstraintBones::GetNiceName() const
+{
+	return FText(LOCTEXT("SolverName", "Constraint Bones"));
+}
+
+bool UIKRig_ConstraintBones::IsBoneAffectedBySolver(const FName& BoneName, const FIKRigSkeleton& IKRigSkeleton) const
+{
+	for (auto& constraintBone : ConstraintBones)
+	{
+		if (constraintBone.ModifiedBone == BoneName)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool UIKRig_ConstraintBones::GetWarningMessage(FText& OutWarningMessage) const
+{
+	if (ConstraintBones.Num() <= 0)
+	{
+		OutWarningMessage = LOCTEXT("NoConstraintBones", "Missing constraint bones.");
+		return true;
+	}
+
+	return false;
+}
+
+bool UIKRig_ConstraintBones::IsGoalConnected(const FName& GoalName) const
+{
+	// always return true as we don't want to connect IK goals for this solver ;)
+	return true;
+}
+
+#endif
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/TTToolbox/Public/IKRig_ConstraintBones.h
@@ -0,0 +1,78 @@
+// The MIT License (MIT)
+// ---------------------
+// 
+// Copyright 2022 Achim Turan (https://www.instagram.com/tuatec/)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+// THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#pragma once
+
+// Unreal Engine includes
+#include "CoreMinimal.h"
+#include "IKRigSolver.h"
+
+#include "IKRig_ConstraintBones.generated.h"
+
+
+USTRUCT(Blueprintable)
+struct TTTOOLBOX_API FConstraintBone
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Constraint)
+	FName ConstraintBone = NAME_None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Constraint)
+	FName ModifiedBone = NAME_None;
+};
+
+UCLASS(EditInlineNew)
+class TTTOOLBOX_API UIKRig_ConstraintBones : public UIKRigSolver
+{
+	GENERATED_BODY()
+
+public:
+	UIKRig_ConstraintBones();
+	~UIKRig_ConstraintBones();
+
+protected: // exposed members
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	TArray<FConstraintBone> ConstraintBones;
+
+protected: // UIKRigSolver interface
+	void Initialize(const FIKRigSkeleton& IKRigSkeleton) override;
+	void Solve(FIKRigSkeleton& IKRigSkeleton, const FIKRigGoalContainer& Goals) override;
+
+#if WITH_EDITOR
+	FText GetNiceName() const override;
+	bool IsBoneAffectedBySolver(const FName& BoneName, const FIKRigSkeleton& IKRigSkeleton) const override;
+	bool GetWarningMessage(FText& OutWarningMessage) const override;
+	bool IsGoalConnected(const FName& GoalName) const override;
+
+	void AddGoal(const UIKRigEffectorGoal& NewGoal) override {}
+	void RemoveGoal(const FName& GoalName) override {}
+	void RenameGoal(const FName& OldName, const FName& NewName) override {}
+	void SetGoalBone(const FName& GoalName, const FName& NewBoneName) override {}
+#endif
+
+private:
+	struct CConstrainedBone
+	{
+		int32 ConstraintBone = INDEX_NONE;
+		int32 ModifiedBone = INDEX_NONE;
+	};
+	TArray<CConstrainedBone> m_constraintBones;
+};
